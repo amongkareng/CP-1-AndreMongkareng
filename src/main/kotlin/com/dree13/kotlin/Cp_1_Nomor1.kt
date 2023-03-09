cec16287159f2c67d7f2aeb4ffbe445f8e9509e2
dree <andremongkareng013@gmail.com>
2023-03-09 11:39:04 +0800

    Initial commit

---
+++ src/main/kotlin/com/dree13/kotlin/Cp_1_Nomor1.kt
@@ -0,0 +1,32 @@
+package com.dree13.kotlin
+/*
+* nomor 1 tentang fungsi rekursif untuk menentukan apakah itu bilangan prima atau bukan
+* // bilangan prima merupakan bilangan yang hanya bisa dibagi 1 dan nilai itu sendiri
+* */
+
+fun isPrime(x : Int, divisor: Int = 2) : Boolean {
+    if (x <= 2) return x == 2
+    if (x / divisor == 0) return false
+    if (divisor * divisor > x) return false
+    return  isPrime(x , divisor + 1)
+//    if (x / x == 1 && x / 1 == x) {
+//        return true
+//    } else{
+//        return false
+//    }
+
+}
+
+fun main(){
+    // untuk fungsi rekursif mencari nilai bilangan prima
+    val x = 16
+    var case = 0
+    if (isPrime(x)){
+        ++case
+        println("case #$case : $x is a prime number")
+    }else{
+        ++case
+        println("case #$case : $x is not a prime number")
+    }
+
+}
---
+++ src/main/kotlin/com/dree13/kotlin/Cp_1_Nomor2.kt
@@ -0,0 +1,15 @@
+package com.dree13.kotlin
+
+fun main(){
+    val N = IntArray(100)
+    val nomorSoal = listOf<Int>(1,2,1,3,4,6,2,3,5,5)
+    val A = charArrayOf('A','A','C','B','D','E')
+    for(i in nomorSoal.indices){
+        N[i] = nomorSoal[i]
+    }
+    val sortedNomorSoal = nomorSoal.sorted().distinct()
+    for (i in sortedNomorSoal.indices) {
+        println("${sortedNomorSoal[i]} answers are ${A[i]}")
+    }
+
+}
